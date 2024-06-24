36439faa3ee2edb7e4fe242065aaed7eba83f90f
itsnikhilesh <160389826+itsnikhilesh@users.noreply.github.com>
2024-06-24 10:26:39 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class third
+{
+    
+   public static void main(String arg[])
+   {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("");
+    String Name = sc.nextLine();
+
+    System.out.print("");
+    String Nme =sc.nextLine();
+
+
+    String name;
+    name= Name +" + " + Nme  ;
+
+    System.out.print(""+name);
+
+
+
+   }
+}
