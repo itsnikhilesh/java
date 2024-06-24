f54d6a9cf63627d6ebca62281e494a2bae267e1b
itsnikhilesh <160389826+itsnikhilesh@users.noreply.github.com>
2024-06-24 10:25:47 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class simple
+{
+    public static void main(String args[])
+    {
+        Scanner myobj =new Scanner(System.in);
+
+        System.out.print("enter username :   ");
+
+         String username = myobj.nextLine();
+       System.out.println("it is your username  :    "+username);
+    }
+}
+    
