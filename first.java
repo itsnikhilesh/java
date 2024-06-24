8b796051fa02a16e99bb8a51a193e55e52f32126
itsnikhilesh <160389826+itsnikhilesh@users.noreply.github.com>
2024-06-24 10:21:41 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class first{
+    public static void main(String []args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the first Number   :");
+    int x =sc.nextInt();
+    
+
+    System.out.print("Enter the second Number  :");
+    int y = sc.nextInt();
+
+    int z;
+        z=x+y;
+        System.out.print("x+y =  :  "+z);
+}
+    
+}
